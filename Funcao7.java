4887b4b86075eb50283d43d6e20c5a5240d3c304
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 18:24:13 -0300

    Add files via upload

---
+++ Funcao7.java
@@ -0,0 +1,22 @@
+/*
+Atividade 4
+
+Criar uma função que recebe um vetor e inverte a ordem de seus elementos
+ */
+package funcao7;
+    public class Funcao7 {
+        static int[] inverte(int vet1[]){
+        int vet2[] = new int [vet1.length];
+            for(int i = 0, j = vet2.length -1;i < vet1.length; i++, j--)
+                vet2[j] = vet1[i];
+        return vet2;
+        }
+        public static void main(String[] args) {
+            int vetnormal[] = {5,6,7,8,9};
+            int vetinvert[] = inverte(vetnormal);
+        
+        for(int i = 0; i<vetinvert.length;i++){
+            System.out.println(vetinvert[i]);
+        }
+    }
+}
